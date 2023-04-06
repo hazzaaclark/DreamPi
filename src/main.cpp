135c9b6b4662ef4c2bfe87e37ba429bfd35726f1
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-04-06 20:37:10 +0100

    Update main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* SEGA Dreamcast Raspberry Pi Pico Interface */
+
+#include "common.h"
+#include "maple.h"
+
+int main(int argc, char** argv[])
+{
+	argc;
+	argv;
+	return 0;
+}
